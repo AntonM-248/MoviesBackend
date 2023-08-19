2ab868448229f359cbe0b350363c8c82f7c597e7
AntonM-248 <atnmku@gmail.com>
2023-08-19 10:25:26 -0400

    Movies Backend Initial Commit

---
+++ src/test/java/inc/mar/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package inc.mar.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
